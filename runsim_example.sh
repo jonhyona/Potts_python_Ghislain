8aebe2df0a501c7ce5ab3da70fef49539a1ce89d
gslndlb <ghislain.delabbey@live.fr>
2020-05-26 12:16:59 +0200

    Example runsim_example.sh

--- runsim_example.sh
+++ runsim_example.sh
@@ -1,3 +1,5 @@
+echo 'Create network'
 parallel 'python3 create_pkl_file.py 0 {1} {2}' :::  0. 0.5 1. ::: 10000
-parallel --bar --shuf -j 3 'cd PottsModel ; python3 run.py {1} {2} {3}' ::: {0..10} ::: 0. 0.5 1. ::: 10000
+echo 'Run simulations'
+parallel --bar --shuf -j 3 'cd PottsModel ; python3 run.py {1} {2} {3}' ::: {0..5} ::: 0. 0.5 1. ::: 1000
 python3 update_sim_table.py
