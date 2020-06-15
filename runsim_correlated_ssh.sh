990b1f29c888bb5ddcae6076815dff4652ebb128
gslndlb <ghislain.delabbey@live.fr>
2020-06-15 16:23:01 +0200

    Progress on coactivation and others

--- runsim_correlated_ssh.sh
+++ runsim_correlated_ssh.sh
@@ -1,6 +1,6 @@
 rsync -a "$(pwd -P)" gdelabbey@dromon.ens.fr: --exclude=".*" --exclude="*.pkl" --exclude "*.png" --exclude="*.txt"
 ssh -t gdelabbey@dromon.ens.fr 'cd PottsModel/data_analysis ; rm -R -- */ ; rm *.txt ; rm *.pkl'
-parallel --sshloginfile nodefile_soft 'cd PottsModel ; pwd ; hostname ; cat /proc/cpuinfo | grep "name" ; python3 generate_network_patterns_in_file.py 0 {1} 100000 2021 {2} {3}' :::  0. 0.5 1. :::+ 1. 1.2 1.4 ::: 0. 0.05 0.1 0.2 0.4
-# parallel --bar --shuf -j 3 --sshloginfile nodefile 'cd PottsModel ; python3 run.py {1} {2} 100000 2021 {3} {4}' ::: {0..199} ::: 0. 0.5 1. :::+ 1. 1.2 1.4 ::: 0. 0.05 0.1 0.2 0.4
+parallel --sshloginfile nodefile_soft 'cd PottsModel ; pwd ; hostname ; cat /proc/cpuinfo | grep "name" ; python3 generate_network_patterns_in_file.py 0 {1} 10000 2021 {2} {3}' :::  0. 0.5 1. :::+ 1. 1.2 1.4 ::: 0. 0.05 0.1 0.2 0.4
+parallel --bar --shuf -j 3 --sshloginfile nodefile 'cd PottsModel ; python3 run.py {1} {2} 1000 2021 {3} {4}' ::: {0..0} ::: 0. 0.5 1. :::+ 1. 1.2 1.4 ::: 0. 0.05 0.1 0.2 0.4
 rsync -v -r gdelabbey@dromon.ens.fr:PottsModel/data_analysis/ data_analysis/
 python3 update_sim_table.py
